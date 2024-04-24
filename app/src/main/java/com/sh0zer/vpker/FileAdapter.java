fe00909db6cd941626fa35c8b4ffcc2e0543637a
klaymen1n <84157659+klaymen1n@users.noreply.github.com>
2024-04-25 00:05:35 +0300

    idontwannadescribewhatichanged

--- app/src/main/java/com/sh0zer/vpker/FileAdapter.java
+++ app/src/main/java/com/sh0zer/vpker/FileAdapter.java
@@ -15,34 +15,56 @@ import java.util.Collections;
 public class FileAdapter extends ArrayAdapter<File> {
     private ArrayList<File> fileList;
     private LayoutInflater inflater;
+    private Context context;
+    private static final int VIEW_TYPE_BACK_BUTTON = 0;
+    private static final int VIEW_TYPE_FILE = 1;
 
     public FileAdapter(Context context, ArrayList<File> fileList) {
         super(context, 0, fileList);
         this.fileList = fileList;
+        this.context = context;
         inflater = LayoutInflater.from(context);
 
         Collections.sort(fileList, (o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+
+        fileList.add(0, null);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_BACK_BUTTON : VIEW_TYPE_FILE;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        int viewType = getItemViewType(position);
+
         if (convertView == null) {
-            convertView = inflater.inflate(R.layout.list_item, parent, false);
+            if (viewType == VIEW_TYPE_BACK_BUTTON) {
+                convertView = inflater.inflate(R.layout.back_button_layout, parent, false);
+            } else {
+                convertView = inflater.inflate(R.layout.list_item, parent, false);
+            }
         }
 
-        File file = fileList.get(position);
+        if(viewType == VIEW_TYPE_BACK_BUTTON)
+            return convertView;
 
+        File file = fileList.get(position);
         TextView textView = convertView.findViewById(android.R.id.text1);
         textView.setText(file.getName());
-
         ImageView imageView = convertView.findViewById(R.id.imageView);
-
-        if(file.isDirectory())
+        if (file.isDirectory()) {
             imageView.setImageResource(R.drawable.folder);
-        else
+        } else {
             imageView.setImageResource(R.drawable.file);
+        }
 
         return convertView;
     }
-
 }
--- app/src/main/java/com/sh0zer/vpker/MainActivity.java
+++ app/src/main/java/com/sh0zer/vpker/MainActivity.java
@@ -1,21 +1,28 @@
 package com.sh0zer.vpker;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.connorhaigh.javavpk.core.Archive;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    Archive archive;
 
     private static final int REQUEST_PERMISSION = 100;
     private ListView fileListView;
@@ -40,13 +47,23 @@ public class MainActivity extends Activity {
                     REQUEST_PERMISSION);
         else {
             File initialDirectory = Environment.getExternalStorageDirectory();
-            displayFiles(initialDirectory);
             currentDirectory = initialDirectory;
+            displayFiles(initialDirectory);
         }
+
+        ImageView menuButton = findViewById(R.id.menuIcon);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenu();
+            }
+        });
 
         fileListView.setOnItemClickListener((parent, view, position, id) -> {
             File selectedFile = fileList.get(position);
-            if (selectedFile.isDirectory()) {
+            if(position == 0)
+                goBack();
+            else if (selectedFile.isDirectory()) {
                 displayFiles(selectedFile);
                 currentDirectory = selectedFile;
             } else {
@@ -56,33 +73,13 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_menu:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(R.string.about_text)
-                        .setTitle(R.string.action_about);
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 File initialDirectory = Environment.getExternalStorageDirectory();
-                displayFiles(initialDirectory);
                 currentDirectory = initialDirectory;
+                displayFiles(initialDirectory);
             } else {
                 Toast.makeText(this, "No permissions", Toast.LENGTH_SHORT).show();
             }
@@ -101,23 +98,17 @@ public class MainActivity extends Activity {
                     Toast.makeText(this, "Do it twice to exit", Toast.LENGTH_SHORT).show();
                 }
             }
-            else{
-                try {
-                    displayFiles(currentDirectory.getParentFile());
-                    currentDirectory = currentDirectory.getParentFile();
-                    retries = 0;
-                }catch (Exception e) {
-                    Log.e("ERROR", "Error has occurred " + e.getMessage());
-                }
-            }
+            else
+                goBack();
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private void displayFiles(File directory) {
-        fileList.clear();
         File[] files = directory.listFiles();
 
         if (files != null) {
+            fileList.clear();
             for (File file : files) {
                 if (file.isFile() || file.isDirectory()) {
                     fileList.add(file);
@@ -125,6 +116,57 @@ public class MainActivity extends Activity {
             }
             FileAdapter adapter = new FileAdapter(this, fileList);
             fileListView.setAdapter(adapter);
+
+            TextView emptyText = findViewById(R.id.emptyText);
+            if(fileList.size() == 1)
+                emptyText.setVisibility(View.VISIBLE);
+            else
+                emptyText.setVisibility(View.GONE);
+
+
+            TextView directoryText =  findViewById(R.id.directoryText);
+            String text = directory.toString();
+            if(text.length() > 50) {
+                String temp1 = text.substring(text.length() - 50);
+                String temp2 = temp1.substring(temp1.indexOf("/") + 1);
+                directoryText.setText("Current directory is ../" + temp2);
+            }
+            else
+                directoryText.setText("Current directory is " + text);
         }
     }
-}
+
+    void showMenu(){
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, findViewById(R.id.menuIcon));
+        popupMenu.getMenuInflater().inflate(R.menu.menu_main, popupMenu.getMenu());
+        popupMenu.show();
+
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_menu) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(R.string.about_text)
+                            .setTitle(R.string.action_about);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return true;
+                }
+                else
+                    return false;
+            }
+        });
+        }
+
+        void goBack()
+        {
+            try {
+                displayFiles(currentDirectory.getParentFile());
+                currentDirectory = currentDirectory.getParentFile();
+                retries = 0;
+            }catch (Exception e) {
+                Log.e("ERROR", "Error has occurred " + e.getMessage());
+            }
+        }
+    }
